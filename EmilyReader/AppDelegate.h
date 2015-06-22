c348dfb5d3f04fc6c50dd6570c85ec14355b596c
James <trask@hp.com>
2015-06-22 15:31:48 -0700

    Initial Commit

---
+++ EmilyReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmilyReader
+//
+//  Created by James Trask on 6/22/15.
+//  Copyright (c) 2015 Code Joy Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmilyReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmilyReader
+//
+//  Created by James Trask on 6/22/15.
+//  Copyright (c) 2015 Code Joy Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
